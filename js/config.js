01222b2c8f20b094f7248ee27fbb48330a245b61
hs5743 <hs5743@users.noreply.github.com>
2026-07-04 08:24:53 +0800

    Update handover latest version metadata to v1.5.3-CP18

--- js/config.js
+++ js/config.js
@@ -140,7 +140,7 @@ window.SupabaseConfig = {
 }
 
 // 系統版本號
-const APP_VERSION = 'v1.5.2-CP17'
+const APP_VERSION = 'v1.5.3-CP18'
 
 function appendAppVersion() {
   const footers = document.getElementsByTagName('footer')
